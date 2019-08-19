ce6f59c2e6cdb4fe0e78becd1ac7c2bccf28ebfe
emoises <51842925+emoises@users.noreply.github.com>
2019-08-19 13:11:24 -0300

    create pt.js
    I translated the model with answers to portuguese.

---
+++ lambda/custom/languages/pt.js
@@ -0,0 +1,20 @@
+module.exports = {
+  translation: {
+    SKILL_NAME: 'adivinha número',
+    EXIT_MESSAGE: 'Obrigado por jogar!',
+    FALLBACK_MESSAGE_DURING_GAME: `Não posso ajudar com isso.  Tente adivinhar um número de 0 a 100. `,
+    FALLBACK_REPROMPT_DURING_GAME: 'Por favor adivinhe um número de 0 a 100.',
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `Não consigo te ajudar com isso. Eu vou pegar um númeor de 0 a 100 e você tenta adivinhar. Gostaria de tentar?`,
+    GUESS_CORRECT_MESSAGE: '%s está correto! Gostaria de tentar novamente?',
+    LAUNCH_MESSAGE: 'Bem vindo ao adivinha número. você jogou %s vezes. Gostaria de jogar?',
+    TOO_HIGH_MESSAGE: '%s é muito alto.',
+    TOO_HIGH_REPROMPT: 'Tente um número menor.',
+    TOO_LOW_MESSAGE: '%s é muito baixo.',
+    TOO_LOW_REPROMPT: 'Tente um número maior.',
+    CONTINUE_MESSAGE: 'Diga sim para jogar ou não para sair.',
+    HELP_MESSAGE: 'Estou pensando em um número entre zero e cem, Tente adivinhar, eu direi se está muito alot ou baixo.',
+    HELP_REPROMPT: 'Tente dizer um número.',
+    ERROR_MESSAGE: 'Desculpe, ocorreu um erro.',
+    YES_MESSAGE: 'Muito bom! Tente dizer um número para começarmos o jogo.'
+  }
+};
